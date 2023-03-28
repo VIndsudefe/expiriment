48e20438126b674e306c3c42ab45223fab9f069b
Aalam <otasmatov@gmail.com>
2023-03-28 16:02:57 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+import Footer from './Footer';
+import Header from './Header';
+import Home from './Home';
+
+function App() {
+  
+
+
+  return (
+    <div className="App">
+      <Header />
+      <Home title="lalala"/>
+      <Home title='bebebe'/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
